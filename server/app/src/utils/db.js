43654c3ae6f71ddffca23647d2e1b2362f600cb0
vorobeybird <vorobeybird@gmail.com>
2021-10-21 14:42:47 +0300

    JST4-60: add temporary createConnection func

--- server/app/src/utils/db.js
+++ server/app/src/utils/db.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 require("dotenv").config();
 
-const connection = mysql.createPool({
+const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
@@ -10,63 +10,62 @@ const connection = mysql.createPool({
   multipleStatements: true,
 });
 
-// connection.connect((error) => {
-//   if (error) throw error;
-//   // temporary - to create table dishes
-//   const sql = `
-//     CREATE TABLE IF NOT EXISTS dishes (
-//       id INT NOT NULL  AUTO_INCREMENT,
-//        title VARCHAR(100),
-//         default_ingredients VARCHAR(200),
-//           price INT,
-//            weight INT,
-//             calories INT,
-//              PRIMARY KEY (id)) ENGINE=INNODB;
-//              CREATE TABLE IF NOT EXISTS dishes_photos (
-//               id INT NOT NULL AUTO_INCREMENT,
-//               dish_id INT NOT NULL,
-//               photo_url VARCHAR(50) NOT NULL,
-//               ordinal_num INT,
-//               width INT,
-//               height INT,
-//               PRIMARY KEY (id),
-//               FOREIGN KEY (dish_id) REFERENCES dishes(id)
-//               ON DELETE CASCADE
-//             ) ENGINE=INNODB;
-//             CREATE TABLE IF NOT EXISTS ingredients (
-//               id INT NOT NULL AUTO_INCREMENT,
-//               title VARCHAR(30) NOT NULL,
-//               PRIMARY KEY (id)
-//             ) ENGINE=INNODB;
-//             CREATE TABLE IF NOT EXISTS dishes_ingredients (
-//               id INT NOT NULL AUTO_INCREMENT,
-//               dish_id INT NOT NULL,
-//               ingredient_id INT NOT NULL,
-//               PRIMARY KEY (id),
-//               FOREIGN KEY (dish_id) REFERENCES dishes(id)
-//               ON DELETE CASCADE            
-//             ) ENGINE=INNODB;
-//             CREATE TABLE IF NOT EXISTS categories (
-//               id INT NOT NULL AUTO_INCREMENT,
-//               title VARCHAR(30) NOT NULL,
-//               PRIMARY KEY (id)
-//             ) ENGINE=INNODB;
-//             CREATE TABLE IF NOT EXISTS dishes_categories (
-//               id INT NOT NULL AUTO_INCREMENT,
-//               dish_id INT NOT NULL,
-//               category_id INT NOT NULL,
-//               PRIMARY KEY (id),
-//               FOREIGN KEY (dish_id) REFERENCES dishes(id)
-//               ON DELETE CASCADE
-//             ) ENGINE=INNODB;
+connection.connect((error) => {
+  if (error) throw error;
+  // temporary - to create table dishes
+  const sql = `
+    CREATE TABLE IF NOT EXISTS dishes (
+      id INT NOT NULL  AUTO_INCREMENT,
+       title VARCHAR(100),
+        default_ingredients VARCHAR(200),
+          price INT,
+           weight INT,
+            calories INT,
+             PRIMARY KEY (id)) ENGINE=INNODB;
+             CREATE TABLE IF NOT EXISTS dishes_photos (
+              id INT NOT NULL AUTO_INCREMENT,
+              dish_id INT NOT NULL,
+              photo_url VARCHAR(50) NOT NULL,
+              ordinal_num INT,
+              width INT,
+              height INT,
+              PRIMARY KEY (id),
+              FOREIGN KEY (dish_id) REFERENCES dishes(id)
+              ON DELETE CASCADE
+            ) ENGINE=INNODB;
+            CREATE TABLE IF NOT EXISTS ingredients (
+              id INT NOT NULL AUTO_INCREMENT,
+              title VARCHAR(30) NOT NULL,
+              PRIMARY KEY (id)
+            ) ENGINE=INNODB;
+            CREATE TABLE IF NOT EXISTS dishes_ingredients (
+              id INT NOT NULL AUTO_INCREMENT,
+              dish_id INT NOT NULL,
+              ingredient_id INT NOT NULL,
+              PRIMARY KEY (id),
+              FOREIGN KEY (dish_id) REFERENCES dishes(id)
+              ON DELETE CASCADE            
+            ) ENGINE=INNODB;
+            CREATE TABLE IF NOT EXISTS categories (
+              id INT NOT NULL AUTO_INCREMENT,
+              title VARCHAR(30) NOT NULL,
+              PRIMARY KEY (id)
+            ) ENGINE=INNODB;
+            CREATE TABLE IF NOT EXISTS dishes_categories (
+              id INT NOT NULL AUTO_INCREMENT,
+              dish_id INT NOT NULL,
+              category_id INT NOT NULL,
+              PRIMARY KEY (id),
+              FOREIGN KEY (dish_id) REFERENCES dishes(id)
+              ON DELETE CASCADE
+            ) ENGINE=INNODB;
 
-        
-//     `;
-//   connection.query(sql, (err, result) => {
-//     if (err) throw err;
-//     result.warningCount === 0 && console.log("Lacking Table dishes created.");
-//   });
-//   // !temporary - to create table dishes
-//   console.log("Successfully connected to the database.");
-// });
+    `;
+  connection.query(sql, (err, result) => {
+    if (err) throw err;
+    result.warningCount === 0 && console.log("Lacking Table dishes created.");
+  });
+  // !temporary - to create table dishes
+  console.log("Successfully connected to the database.");
+});
 module.exports = connection;
