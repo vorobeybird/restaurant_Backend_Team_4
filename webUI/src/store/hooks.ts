1d77b58cc2b2d4674994e1dd37fc374a4ecdae3b
Vadim Fedorovich <vadimfedorovich22@mail.ru>
2021-10-25 17:15:15 +0300

    resolve conflicts

--- webUI/src/store/hooks.ts
+++ webUI/src/store/hooks.ts
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector, TypedUseSelectorHook } from "react-redux";
 import { AppDispatch } from ".";
-import { RootState } from ".";
+import { AppStateType } from ".";
 
 export const useAppDispatch = () => useDispatch<AppDispatch>()
 
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useAppSelector: TypedUseSelectorHook<AppStateType  > = useSelector
