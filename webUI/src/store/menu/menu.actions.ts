20bf0a3e94f1c1d6b74ca9e741d595217e270163
Siarhei Korbut <Siarhei.Korbut@itechart-group.com>
2021-10-27 17:04:28 +0300

    JST4-61-fix-page-layout added responsive layout up to 768 pixels

--- webUI/src/store/menu/menu.actions.ts
+++ webUI/src/store/menu/menu.actions.ts
@@ -4,7 +4,7 @@ import axios, { AxiosResponse } from "axios"
 import { Response } from './menu.types';
 
 const getItems = async () => {
-  const response = await axios.get('http://ec2-18-192-170-78.eu-central-1.compute.amazonaws.com:5000/api/dish') as AxiosResponse<Response>
+  const response = await axios.get('http://18.192.61.153:5000/api/dish') as AxiosResponse<Response>
   return response.data
 }
 
@@ -13,3 +13,5 @@ export const fetchMenuItems =  () => async (dispatch: AppDispatch) => {
   const items = await getItems()
   dispatch({type: MenuConstants.FETCH_ITEMS, payload: items.data})
 }
+
+// http://ec2-18-192-170-78.eu-central-1.compute.amazonaws.com:5000/api/dish'
