1f653d7d20c35ac49fe3f9bc8e34e46512232156
Siarhei Korbut <Siarhei.Korbut@itechart-group.com>
2021-10-29 15:48:27 +0300

    JST4-61-fix-page-layout ref added dotenv file with url varaible to use it in requests

--- webUI/src/store/menu/menu.actions.ts
+++ webUI/src/store/menu/menu.actions.ts
@@ -3,15 +3,14 @@ import { AppDispatch } from '../../store';
 import axios, { AxiosResponse } from "axios"
 import { Response } from './menu.types';
 
+const GET_DISHES_URL : any = `${process.env.REACT_APP_GET_DISHES}/api/dish`; 
+
 const getItems = async () => {
-  const response = await axios.get('http://18.192.61.153:5000/api/dish') as AxiosResponse<Response>
+  const response = await axios.get(GET_DISHES_URL) as AxiosResponse<Response>
   return response.data
 }
-
 
 export const fetchMenuItems =  () => async (dispatch: AppDispatch) => {
   const items = await getItems()
   dispatch({type: MenuConstants.FETCH_ITEMS, payload: items.data})
 }
-
-// http://ec2-18-192-170-78.eu-central-1.compute.amazonaws.com:5000/api/dish'
