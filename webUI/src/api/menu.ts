51efaa67f1915436d7c4c921a82a569a534f6083
nebiros88 <nebiros_88@mail.ru>
2021-10-31 15:44:24 +0300

    JST4-61-fix-page-layout ref unused files have been deleted and .env file with default variable value have been added to remote repo

--- webUI/src/api/menu.ts
+++
@@ -1,7 +0,0 @@
-import axios, { AxiosResponse } from "axios"
-import { Response } from "../features/menu/menu.types"
-
-export const getItems = async () => {
-  const response = await axios.get('') as AxiosResponse<Response>
-  return response.data
-}
