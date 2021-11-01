3a7273f7d0a23ea13c76927595affcc10cc671d8
artur <me@zalart.com>
2021-11-01 10:49:37 +0300

    JST4-96-Adding-dish-pucture. Delet all images endpoint added

--- server/app/src/resources/image/imageRouter.js
+++ server/app/src/resources/image/imageRouter.js
@@ -6,7 +6,7 @@ const path = require("path");
 const upload = multer({ dest: "dishesphotos/" });
 router.route("/").post(upload.single("image"), imageController.uploadImage);
 //router.route("/:imageId").get(imageController.downloadById);
-//router.route("/all/:dishId").delete(imageController.deleteAllImages);
+router.route("/all/:dishId").delete(imageController.deleteAllImages);
 router.route("/:publicId").delete(imageController.deleteById);
 
 module.exports = router;
