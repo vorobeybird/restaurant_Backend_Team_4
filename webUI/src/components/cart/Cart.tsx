b8b350659dc8b65bbba24915f7d0a07ba910efb9
Gnarll <vadimfedorovich22@mail.ru>
2021-11-01 22:33:53 +0300

    implement dish order

--- webUI/src/components/cart/Cart.tsx
+++ webUI/src/components/cart/Cart.tsx
@@ -7,21 +7,58 @@ import { MenuItem } from "../../store/menu/menu.types";
 import { useEffect, useState } from "react";
 import { ICartItem } from "../../store/cart/cart.types";
 import { Button } from "../common/button/Button";
+import axios, { AxiosResponse } from "axios";
 
 export const Cart = () => {
   const items = useAppSelector((state) => state.cartItems.items);
+  const userId = useAppSelector((state) => state.auth.user.attributes.sub);
   const totalPrice = items.reduce((acc, el) => acc + el.price * el.amount, 0);
 
-  const onBookTable = () => {
-    console.log("table is booked");
-  };
+  interface DishShortInfo {
+    dish_id: number;
+    dish_amount: number;
+  }
 
-  const onDelivery = () => {
-    console.log("lunch is running!");
-  };
+  interface Order {
+    adress: string;
+    customer_id: string;
+    delivery_method: string;
+    total_price: number;
+    delivery_date: Date;
+    contact_info: string;
+    payment_method: boolean;
+    comment: string;
+    dish: DishShortInfo[];
+  }
 
-  const onTakeaway = () => {
-    console.log("takeaway");
+  const onMakingOrder = () => {
+    let order = {} as Order;
+    order.adress = "Brest";
+    order.customer_id = userId;
+    order.delivery_method = "Самовывоз";
+    order.total_price = totalPrice;
+    order.delivery_date = new Date();
+    order.contact_info = "EdgarAllanPoe +375666666666";
+
+    order.payment_method = true;
+    order.comment = "Hi, I'm hardcode comment :)";
+
+    let dishesShortInfo = items.map((item) => {
+      let dish = {} as DishShortInfo;
+      dish.dish_id = item.id;
+      dish.dish_amount = item.amount;
+      return dish;
+    });
+
+    order.dish = dishesShortInfo;
+
+    axios
+      .post("http://localhost:5000/api", order, {
+        headers: { "Content-type": "application/json" },
+      })
+      .then((response) => console.log(response))
+      .catch((err) => console.log(err));
+    console.log(order);
   };
 
   return (
@@ -38,25 +75,10 @@ export const Cart = () => {
             <CartItem key={index} {...item} />
           ))}
           <div>Итого: {totalPrice} BYN</div>
-          <div className="order_actions">
-            <div>Тип заказа: </div>
-            <div className="order_buttons">
-              <div>
-                <Button type="button" onClick={onBookTable}>
-                  Забронировать стол
-                </Button>
-              </div>
-              <div>
-                <Button type="button" onClick={onDelivery}>
-                  Доставка
-                </Button>
-              </div>
-              <div>
-                <Button type="button" onClick={onTakeaway}>
-                  Навынос
-                </Button>
-              </div>
-            </div>
+          <div className="make_order">
+            <Button type="button" onClick={onMakingOrder}>
+              Оформить Заказ
+            </Button>
           </div>
         </>
       )}
