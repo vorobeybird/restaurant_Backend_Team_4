29756d13b78b212cfbba7aa29447b3fc32bce3ec
Varabei Kanstantsin <vorobeybird@gmail.com>
2021-11-16 15:18:16 +0300

    fix merge issue

--- webUI/src/components/common/chooseDate/ChooseDate.tsx
+++ webUI/src/components/common/chooseDate/ChooseDate.tsx
@@ -1,31 +1,29 @@
-
 import "./chooseDate.scss";
 import Calendar from "react-calendar";
-import "react-calendar/dist/Calendar.css";
+// import "react-calendar/dist/Calendar.css";
 import { useAppDispatch, useAppSelector } from "../../../store/hooks";
 import { changeDate } from "../../../store/order/order.actions";
 
+export const ChooseDate = () => {
+  const date = useAppSelector((state) => state.order.order.delivery_date);
 
-  export const ChooseDate = () => {
-    const date = useAppSelector((state) => state.order.order.delivery_date);
+  const dispatch = useAppDispatch();
 
-    const dispatch = useAppDispatch();
-
-    const onChangeData = (value: Date) => {
-      value.setMinutes(date.getMinutes());
-      value.setHours(date.getHours());
-      console.log(value, "value");
-      dispatch(changeDate(value));
-    };
-
-    return (
-        <>
-        <div className="order-header">Дата</div>
-        <div className="choose_date">
-          <div>
-            <Calendar onChange={onChangeData} value={date} prev2Label={null} next2Label={null}/>
-          </div>
-        </div>
-        </>
-    );
+  const onChangeData = (value: Date) => {
+    value.setMinutes(date.getMinutes());
+    value.setHours(date.getHours());
+    console.log(value, "value");
+    dispatch(changeDate(value));
   };
+
+  return (
+      <>
+      <div className="order-header">Дата</div>
+      <div className="choose_date">
+        <div>
+          <Calendar onChange={onChangeData} value={date} prev2Label={null} next2Label={null}/>
+        </div>
+      </div>
+      </>
+  );
+};
--- webUI/src/components/common/chooseTime/ChooseTime.tsx
+++ webUI/src/components/common/chooseTime/ChooseTime.tsx
@@ -1,9 +1,8 @@
-  import { useEffect, useState } from "react";
-  import { useAppDispatch, useAppSelector } from "../../../store/hooks";
-  import { changeTime } from "../../../store/order/order.actions";
-  import { Button } from "../button/Button";
-  import "./chooseTime.scss";
-
+import { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../../../store/hooks";
+import { changeTime } from "../../../store/order/order.actions";
+import { Button } from "../button/Button";
+import "./chooseTime.scss";
 
 const possibleTime = [
   "12:30",
@@ -15,52 +14,51 @@ const possibleTime = [
   "16:30",
   "18:00",
   "20:30",
+  "21:00",
+  "21:30",
+  "22:00",
 ];
 
+interface ChooseTimeProps {
+  time: string;
+  setTime: (time: string) => void;
+}
 
-  interface ChooseTimeProps {
-    time: string;
-    setTime: (time: string) => void;
-  }
+export const ChooseTime = ({ ...props }: ChooseTimeProps) => {
+  const dispatch = useAppDispatch();
+  const date = useAppSelector((state) => state.order.order.delivery_date);
 
-  export const ChooseTime = ({ ...props }: ChooseTimeProps) => {
-    const dispatch = useAppDispatch();
-    const date = useAppSelector((state) => state.order.order.delivery_date);
-
-    const handleChangeTime = (e: any, time: string) => {
-      const [hours, minutes] = time.split(":");
-      dispatch(changeTime(hours, minutes));
-      props.setTime(time);
-    };
-
-
+  const handleChangeTime = (e: any, time: string) => {
+    const [hours, minutes] = time.split(":");
+    dispatch(changeTime(hours, minutes));
+    props.setTime(time);
+  };
 
   return (
     <div className="choose_time_container">
-      <div>Выберите время</div>
+      <div className="order-header" >Время</div>
       <div>
-        <div>Время</div>
         <div className="time_container">
           {possibleTime.map((timeItem) => {
             return (
-              <div className="timebox">
-                <Button
+              <div  className={
+                timeItem === props.time ? "timebox pushed_button" : "timebox"
+              }>
+                <button
+                  className="timebox__button"
                   type="button"
                   name={timeItem}
                   onClick={(e) => handleChangeTime(e, timeItem)}
                 >
-                  <span
-                    className={
-                      timeItem === props.time ? "pushed_button" : undefined
-                    }
-                  >
+                  <span>
                     {timeItem}
                   </span>
-                </Button>
+                </button>
               </div>
             );
           })}
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
