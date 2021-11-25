e34cc062844137b6d3cbba2cfa5114ea59227a3d
Varabei Kanstantsin <vorobeybird@gmail.com>
2021-11-25 08:17:13 +0300

    JST4-196 add google map

--- webUI/src/components/contacts/Contacts.tsx
+++ webUI/src/components/contacts/Contacts.tsx
@@ -16,25 +16,14 @@
             <div className="our-contacts_title">Контакты</div>
             <ul className="our-contacts_list">
               <li>
-                <img src={MapPointer} alt="mapPointer" />
+                {/* <img src={MapPointer} alt="mapPointer" /> */}
+                
                 <div>Минск, Революционная д. 17</div>
               </li>
               <li><img src={Phone} alt="mapPointer" />
                 <div>+375 29 123-23-34</div></li>
               <li><img src={Clock} alt="mapPointer" />
                 <div>Пн- Вс с 8:00 до 23:00</div></li>
-            </ul>
-            <ul className="our-contacts_social">
-              <li>
-                <a href="https://www.instagram.com/">
-                  <img src={Instagram} alt="instagram-logo" />
-                </a>
-              </li>
-              <li>
-                <a href="https://www.facebook.com/">
-                  <img src={Facebook} alt="facebook-logo" />
-                </a>
-              </li>
             </ul>
           </div>
           <div className="our-app">
@@ -46,15 +35,10 @@
                   <img src={GooglePlay} alt="google-play-logo" />
                 </a>
               </li>
-              <li>
-                <a href="https://www.apple.com/ru/app-store/">
-                  <img src={AppStore} alt="app-store-logo" />
-                </a>
-              </li>
             </ul>
           </div>
         </div>
-        <div className="map"></div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9402.7932719204!2d27.54608244970704!3d53.90156584693363!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dbcfe967b4142f%3A0xc70eccdee62c5086!2z0YPQuy4g0KDQtdCy0L7Qu9GO0YbQuNC-0L3QvdCw0Y8gMTcsINCc0LjQvdGB0Lo!5e0!3m2!1sru!2sby!4v1637768488215!5m2!1sru!2sby" width={"1600"} height={"400"} style={{border:0}} loading="lazy"></iframe>
       </div>
     );
   };
